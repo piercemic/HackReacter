63a159f257564b12de2ffad6b6b9325312e63d48
Michael Pierce <pierce.michael.b@gmail.com>
2020-08-14 08:49:55 -0500

    Create Prep_Module2.js

---
+++ Prep_Module2.js
@@ -0,0 +1,17 @@
+/* MODULE 2: 17 Problems */
+
+/* TESTING */
+
+//assertEqual
+function assertEqual(actual, expected, testName) {
+ if (actual === expected) {
+     console.log('passed');
+ } else {
+     console.log(`FAILED [${testName}] Expected "${expected}", but got "${actual}"`);
+ }
+}
+
+/* SKELETONS */
+
+
+/* FASHION INVENTORY */
