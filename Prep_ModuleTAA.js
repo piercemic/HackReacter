f54d16a881cf662aa3924f09b940a28a5c1faaf2
Michael Pierce <pierce.michael.b@gmail.com>
2020-08-15 09:41:14 -0500

    Create Prep_ModuleTAA.js

---
+++ Prep_ModuleTAA.js
@@ -0,0 +1,18 @@
+/* Advanced Pre-Assessment Practice */
+
+//Altitude Deltas - Difficulty: About even with TAA
+
+let output_0 = sumAltitudeDeltas([1, 2, 3, 1], 0, 3);
+console.log(output_0); //--> 6
+
+let output_1 = sumAltitudeDeltas([5, 3, 6, 7, 2], 2, 4);
+console.log(output_1); //--> 7
+
+let output_2 = sumAltitudeDeltas([5, 3, 6, 7, 2], 0, 1);
+console.log(output_2); //--> 2
+
+let output_3 = sumAltitudeDeltas([5, 3, 6, 7, 2], 0, 4);
+console.log(output_3); //--> 15
+
+let output_4 = sumAltitudeDeltas([4, 1, 4, 0, 20, 13], 1, 4);
+console.log(output_4); //--> 50
