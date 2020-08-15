6a3b3a059c28a70b699c3b90ec316317dafe5fe5
Michael Pierce <pierce.michael.b@gmail.com>
2020-08-15 13:57:23 -0500

    Completed up to TAA - Comma Separated Integers

--- Prep_ModuleTAA.js
+++ Prep_ModuleTAA.js
@@ -128,3 +128,37 @@ function generateSampleView(apiCall) {
  ];
  const expected = ["Sincere@april.biz", "Victor Plains, Suite 879, Wisokyburgh, 90566-7771"]
  assertArraysEqual(generateSampleView(users), expected, 'API Data Call - Odds: Email, Evens: Address');
+
+ //Comma Separated Integers - Difficulty: More difficult than the TAA
+ function solution(integers){
+  if (integers.length > 0 && integers.length < 3) {
+    return integers.join(',');
+  }
+  let result = [];
+  let start = 0;
+  let next = 1;
+  let end = 2;
+  while (start < integers.length) {
+    if (integers[end] - integers[next] === 1 && integers[next] - integers[start] === 1) {
+      while (integers[end] - integers[next] === 1) {
+        next = end;
+        end = end + 1;
+      }
+      let seq = integers.slice(start, end);
+      result.push(`${seq[0]}-${seq[seq.length - 1]}`);
+      start = end;
+      next = end + 1;
+      end = end + 2;
+    } else {
+      result.push(integers[start]);
+      start = next;
+      next = end;
+      end = end + 1;
+    }
+  }
+  return result.join(',');
+}
+solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// returns "-6,-3-1,3-5,7-11,14,15,17-20"
+solution([-4, -3, -2, -1, 2, 3, 5, 6, 12, 13, 14, 15, 17]);
+// returns "-4--1,2,3,5,6,12-15,17"
